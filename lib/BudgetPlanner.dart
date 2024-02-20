861ccf383417cefb4d663c40136c9a1b03a24424
ksenia2004k <152779542+ksenia2004k@users.noreply.github.com>
2024-02-20 20:13:11 +0700

    Update BudgetPlanner.dart

--- lib/BudgetPlanner.dart
+++ lib/BudgetPlanner.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget{
         '/ExpensesRoute': (BuildContext context) => const ExpensesRoute(),
         '/PlannerRoute': (BuildContext context) => const PlannerRoute(),
         '/InputRoute': (BuildContext context) => const InputRoute(),
+        '/InputRoute': (BuildContext context) => const InputRoute(),
       }
     );
   }
